c49410466a2c714986b32ec1e406efdb95b0c046
tonorth-dev <du_lidong@qq.com>
2025-02-02 10:30:41 +0800

    学生端完整版

--- lib/common/config_util.dart
+++ lib/common/config_util.dart
@@ -1,13 +1,13 @@
 class ConfigUtil {
-  // static const String baseUrl = "https://admin.81hongshi.com";
-  static const String baseUrl = "http://127.0.0.1";
-  static const String httpPort = "13921";
+  static const String baseUrl = "https://admin.81hongshi.com";
+  // static const String baseUrl = "http://127.0.0.1";
+  static const String httpPort = "";
   static final String fullUrl = _buildFullUrl();
   static const String ossUrl = "https://oss.81hongshi.com"; // todo 暂时使用ip
   static const String ossPort = "9000";
   static const String ossPrefix = "/hongshi";
-  static const String wsUrl = "ws://127.0.0.1";
-  // static const String wsUrl = "ws://47.94.139.86";
+  // static const String wsUrl = "ws://127.0.0.1";
+  static const String wsUrl = "ws://47.94.139.86";
   static const String wsPort = "13921";
 
   static String _buildFullUrl() {
