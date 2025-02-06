bff43d0b60a247ef60c000f51cbb99ec07262a89
tonorth-dev <du_lidong@qq.com>
2025-02-06 19:53:43 +0800

    学生端完整版

--- lib/common/config_util.dart
+++ lib/common/config_util.dart
@@ -1,7 +1,7 @@
 class ConfigUtil {
-  // static const String baseUrl = "https://admin.81hongshi.com";
-  static const String baseUrl = "http://127.0.0.1";
-  static const String httpPort = "13921";
+  static const String baseUrl = "https://admin.81hongshi.com";
+  // static const String baseUrl = "http://127.0.0.1";
+  static const String httpPort = "";
   static final String fullUrl = _buildFullUrl();
   static const String ossUrl = "https://oss.81hongshi.com";
   static const String ossPort = "9000";
